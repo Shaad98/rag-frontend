799942e7c5df7b51c7e5a145e8e517b2a8baea27
Shaad98 <shaadbangi98@gmail.com>
2026-09-03 20:10:15 +0530

    feat(member-frontend): create structured Angular frontend

---
+++ member-frontend/src/app/features/auth/email-verification/email-verification.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-verification',
+  imports: [],
+  templateUrl: './email-verification.html',
+  styleUrl: './email-verification.scss',
+})
+export class EmailVerification {}
---
+++ member-frontend/src/app/features/auth/password-change/password-change.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-change',
+  imports: [],
+  templateUrl: './password-change.html',
+  styleUrl: './password-change.scss',
+})
+export class PasswordChange {}
